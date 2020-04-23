a32186eb1257fee9d27a9c6237997ca0d709ed6b
Vlad Giurca <vladgiurca@gmail.com>
2020-04-23 19:24:06 +0300

    Add public access modifier to Activity, DataAccess, Person classes.

--- Agenda/Agenda.BL/DataAccess.cs
+++ Agenda/Agenda.BL/DataAccess.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Agenda.BL
 {
-    class DataAccess
+    public class DataAccess
     {
     }
 }
--- Agenda/Agenda.BL/Person.cs
+++ Agenda/Agenda.BL/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Agenda.BL
 {
-    class Person
+    public class Person
     {
     }
 }
